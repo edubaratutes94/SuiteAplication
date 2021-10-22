4f8fff744064c63561332308dbe7520ecaaf58c4
yaidel0703 <yaidel0703@gmail.com>
2021-10-22 17:07:40 -0400

    Update models.py

--- SuiteApp/models.py
+++ SuiteApp/models.py
@@ -9,7 +9,7 @@ from django.conf import settings
 from django.db.models.signals import *
 from rest_framework.authtoken.models import Token
 from django.dispatch import receiver
-
+from SuiteApp.utils import *
 
 class UserApp(User):
     uui = models.UUIDField(default=uuid.uuid4, editable=False, null=True)
@@ -33,3 +33,31 @@ class UserApp(User):
 
     class Meta:
         verbose_name_plural = "Usuarios"
+
+
+
+class tblRol(models.Model):
+
+    rol= models.CharField(max_length=25, unique=True, null=False, primary_key=True)
+
+    def __str__(self):
+        return self.rol
+
+
+class registrarUsuario(User):
+    usuario = models.OneToOneField(User, null=False, on_delete=models.CASCADE, parent_link=True)
+    rol = models.ForeignKey(tblRol, on_delete=models.CASCADE)
+    fecha_nacimiento = models.DateField(editable=True)
+    direccion = models.TextField(max_length=150)
+    telefono_particular = models.CharField(max_length=10, validators=[validate_only_numbers])
+    telefono_celular = models.CharField(max_length=8, validators=[validate_only_numbers])
+
+    def __str__(self):
+        return self.usuario.username
+
+    def crearUsuario(sender, instance, created, **kwargs):
+        if created:
+            registrarUsuario.objects.create(usuario=instance)
+
+    def guardarUsuario(sender, instance, **kwargs):
+        instance.registrarusuario.save()
